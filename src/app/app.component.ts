87ec11cd1dfc7f83ecfd83be6436cebd88a334e0
Pedro Tauzen <pedrotauzen@gmail.com>
2024-05-27 21:13:34 +0100

    Added housing location component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {NgOptimizedImage} from "@angular/common";
+import { HomeComponent } from "./home/home.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-root',
