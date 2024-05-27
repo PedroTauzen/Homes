14c082e386b0227fcfaaf575eadb585cdcc0af92
Pedro Tauzen <pedrotauzen@gmail.com>
2024-05-27 21:31:51 +0100

    Added an Input parameter in housing-location.component.ts

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HousingLocation} from "../housing-location";
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
