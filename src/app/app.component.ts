86587c62aaf131c06ae39b84930a573036be0403
Pedro Tauzen <pedrotauzen@gmail.com>
2024-05-28 10:26:14 +0100

    Rolled-back, and then added routes.ts to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NgOptimizedImage } from "@angular/common";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NgOptimizedImage],
+  imports: [RouterOutlet, HomeComponent, NgOptimizedImage, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
